92f2eb8f70fa951c299fa73312df51541a0ef89c
dkmos2016 <dkmos2016@126.com>
2023-12-29 13:40:24 +0800

    for revengeos v11.0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The revengeosOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_leo.mk
+    $(LOCAL_DIR)/revengeos_leo.mk
     
 COMMON_LUNCH_CHOICES := \
-    lineage_leo-user \
-    lineage_leo-userdebug \
-    lineage_leo-eng
+    revengeos_leo-user \
+    revengeos_leo-userdebug \
+    revengeos_leo-eng
